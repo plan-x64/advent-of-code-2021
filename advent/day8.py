e3dc16c1ab87afd7b2c14a70109c73beea9a8ed7
plan-x64 <git@plan64.org>
2021-12-11 19:22:07 -0800

    Solution to AoC day 8 and 9

---
+++ advent/day8.py
@@ -0,0 +1,35 @@
+import adventutils
+import sys
+
+def transform_input(line):
+    raw_str = str(line, "utf-8").strip('\n').split(' ')
+    delimiter_index = raw_str.index('|')
+    return ([set(chars) for chars in raw_str[:delimiter_index]], [set(chars) for chars in raw_str[delimiter_index+1:]])
+
+def decode_line(segments):
+    (examples, code) = segments
+    examples = sorted(examples, key=len)
+    examples[3:6] = sorted(examples[3:6], key=lambda x: (examples[1].issubset(x), len(examples[2].intersection(x))))
+    examples[6:9] = sorted(examples[6:9], key=lambda x: (examples[2].issubset(x), examples[1].issubset(x)))
+    decoded_segments = [examples[index] for index in (7, 0, 3, 5, 2, 4, 6, 1, 9, 8)]
+
+    value = ""
+    for num in [set(chars) for chars in code]:
+        for index, segment in enumerate(decoded_segments):
+            if num == segment:
+                value += str(index)
+    return int(value)
+
+def main():
+    sessionId = sys.argv[1]
+    url = "https://adventofcode.com/2021/day/8/input"
+    input = adventutils.gather_input_data(url, sessionId, transform=transform_input)
+
+    print("Part 1: {}".format(sum([len(list(filter(lambda x: len(x) in (2,3,4,7), val))) for (_, val) in input])))
+    print("Part 2: {}".format(sum([decode_line(line) for line in input])))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Need to pass 'python day8.py sessionId' (Arguments={}, Length={})".format(str(sys.argv), len(sys.argv)))
+    else:  
+        main()
---
+++ advent/day9.py
@@ -0,0 +1,58 @@
+import adventutils
+from collections import deque
+import functools
+import sys
+
+def surrounding(input, x, y):
+    vals = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+    return [(x_i, y_i) for (x_i, y_i) in vals if 0 <= x_i < len(input) and 0 <= y_i < len(input[x])]
+
+def is_min(input, x, y):
+    for (x_i, y_i) in surrounding(input, x, y):
+        if input[x][y] == 9 or input[x_i][y_i] < input[x][y]:
+            return False
+    return True
+
+def find_mins(input):
+    mins = []
+    for row in range(len(input)):
+        for column in range(len(input[row])):
+            if is_min(input, row, column):
+                mins.append((row, column))
+    return mins
+
+def find_basin(input, x, y):
+    basin = []
+    visited = set()
+    queue = deque([(x,y)])
+    
+    while queue:
+        (x_i, y_i) = queue.pop()
+
+        if (x_i, y_i) in visited:
+            continue
+        else:
+            visited.add((x_i, y_i))
+            if input[x_i][y_i] != 9:
+                basin.append((x_i, y_i))
+                queue.extend([(x_j, y_j) for (x_j, y_j) in surrounding(input, x_i, y_i) if (x_j, y_j) not in visited])
+
+    return basin
+
+def main():
+    sessionId = sys.argv[1]
+    url = "https://adventofcode.com/2021/day/9/input"
+    transform = lambda line: [int(char) for char in str(line, "utf-8").strip('\n')]
+    input = adventutils.gather_input_data(url, sessionId, transform)
+
+    mins = find_mins(input)
+    basins = [find_basin(input, x, y) for (x, y) in mins]
+
+    print("Part1: {}".format(sum([input[x][y]+1 for (x,y) in mins])))
+    print("Part2: {}".format(functools.reduce(lambda a,b: a*b, sorted([len(basin) for basin in basins], reverse=True)[0:3])))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Need to pass 'python day9.py sessionId' (Arguments={}, Length={})".format(str(sys.argv), len(sys.argv)))
+    else:  
+        main()
